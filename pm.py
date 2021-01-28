48fe7b6e403feb1ca68c46ad03f5e0e9518ac079
Heikki Jylhä <hjjylh@gmail.com>
2021-01-28 21:03:44 +0200

    added initiate_db function

--- pm.py
+++ pm.py
@@ -1,7 +1,37 @@
 
 import getpass
-from crypto_stuff import encrypt_text, decrypt_text, encrypt_text_list, decrypt_text_list
+from crypto_stuff import load_salt, do_crypto_stuff, encrypt_text, decrypt_text, encrypt_text_list, decrypt_text_list
 from db import add_to_info_table, get_master_table, get_info, get_app_list
+
+
+def initiate_db():
+    from db import reset_db, add_to_master_table
+    from crypto_stuff import generate_salt, load_salt
+    username = getpass.getuser()
+    print("Initiating password database for user" + username + "...\n")
+    reset_db()
+    password_accepted = False
+    while not(password_accepted):
+        master_password = getpass.getpass(prompt='Please enter master password for password manager: ')
+        master_password2 = getpass.getpass(prompt='Please enter the master password again: ')
+        if master_password == master_password2:
+            print("Master password created.\n")
+            password_accepted = True
+        else:
+            print("Passwords you typed are not the same. Please try again.")
+        # print("Generating salt file...")
+        generate_salt()
+        # print("Salt file generated. Please do not remove or modify it.")
+        f = do_crypto_stuff(master_password, load_salt(), 100000)
+        pw_encrypted = encrypt_text(master_password, master_password, f)
+        un_encrypted = encrypt_text(username, master_password, f)
+        add_to_master_table(un_encrypted, pw_encrypted)
+        print("Password database initiated. Following files created:")
+        print("pw.db")
+        print("salt.txt")
+        print("Please do not remove or modify these files outside of this password manager.")
+        print("\n\n")
+        return (master_password, f)
 
 
 # starting text
@@ -10,15 +40,38 @@ print("Welcome to my Password Manager\n")
 # check if DB has been initiated
 master = get_master_table()
 if master == None:
-    # initiate DB
-    # TODO
-    print("Feature not added yet")
-    exit()
+    print("Password database has not been initiated yet.")
+    print("Do you want to initiate it? (Y/N)")
+    answered = False
+    while not(answered):
+        answer = input()
+        if answer.lower() == "y":
+            answered = True
+            print("\n")
+            initiation_data = initiate_db()
+        elif answer.lower() == "n":
+            answered = True
+            exit()
+
+try:
+    master_password = initiation_data[0]
+    f = initiation_data[1]
+except NameError:
+    username = getpass.getuser()
+    master_password = getpass.getpass(prompt='Enter the master password: ')
+    f = do_crypto_stuff(master_password, load_salt(), 100000)
+    # check that they are in the database
+    master_d = decrypt_text_list(master, master_password, f)
+    if not(username == master_d[0] and master_password == master_d[1]):
+        print("Invalid password or username")
+        exit()
+
 
 # username also used for encryption/decryption
-username = getpass.getuser()
+# maybe something better should be used?
+# username = getpass.getuser()
 # ask master password
-master_password = getpass.getpass(prompt='Enter the master password: ')
+# master_password = getpass.getpass(prompt='Enter the master password: ')
 
 # some options how to make use of username and password
 # TODO
@@ -26,13 +79,8 @@ master_password = getpass.getpass(prompt='Enter the master password: ')
 # h_user = username
 # h_pw = master_password
 
-# check that they are in the database
-master_d = decrypt_text_list(master, master_password)
-if not(username == master_d[0] and master_password == master_d[1]):
-    print("Invalid password or username")
-    exit()
 
-print("Password Manager for user", username)
+print("Password Manager for user", username, "\n")
 
 # the program loop
 while True:
