d10abcbe4e52c86d31a6cc243c9afb3a0c28354b
Heikki Jylhä <hjjylh@gmail.com>
2021-01-28 21:02:36 +0200

    changed crypto fcns so that encrypt/decrypt does not always generate encryptor

--- crypto_stuff.py
+++ crypto_stuff.py
@@ -16,34 +16,30 @@ def load_salt():
 # encryption/decryption requires bytes instead of strings
 # so we make the conversion
 # also add salt
-def prepare(text, password):
-    # convert strings to bytes
-    encoded_text = text.encode('utf-8')
-    encoded_pw = password.encode('utf-8')
-    # load the salt
-    local_salt = load_salt()
-    return (encoded_text, encoded_pw, local_salt)
+# def prepare(text, password):
+#     # convert strings to bytes
+#     encoded_text = text.encode('utf-8')
+#     encoded_pw = password.encode('utf-8')
+#     # load the salt
+#     local_salt = load_salt()
+#     return (encoded_text, encoded_pw, local_salt)
 
-def do_crypto_stuff(encoded_password, given_salt, num_of_iterations):
+def do_crypto_stuff(password, given_salt, num_of_iterations):
+    encoded_password = password.encode('utf-8')
     kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=given_salt, iterations=num_of_iterations)
     key = base64.urlsafe_b64encode(kdf.derive(encoded_password))
     return Fernet(key)
 
-def encrypt_text(text, password):
-    prep = prepare(text, password)
-    # do crypto stuff
-    f = do_crypto_stuff(prep[1], prep[2], 100000)
-    # convert bytes to strings
-    return f.encrypt(prep[0]).decode('utf-8')
+def encrypt_text(text, password, fernet_thing):
+    encoded_text = text.encode('utf-8')
+    return fernet_thing.encrypt(encoded_text).decode('utf-8')
 
-def decrypt_text(text, password):
-    prep = prepare(text, password)
-    f = do_crypto_stuff(prep[1], prep[2], 100000)
-    # convert bytes to strings
-    return f.decrypt(prep[0]).decode('utf-8')
+def decrypt_text(text, password, fernet_thing):
+    encoded_text = text.encode('utf-8')
+    return fernet_thing.decrypt(encoded_text).decode('utf-8')
 
-def encrypt_text_list(text_list, password):
-    return [encrypt_text(text, password) for text in text_list]
+def encrypt_text_list(text_list, password, fernet_thing):
+    return [encrypt_text(text, password, fernet_thing) for text in text_list]
 
-def decrypt_text_list(text_list, password):
-    return [decrypt_text(text, password) for text in text_list]
+def decrypt_text_list(text_list, password, fernet_thing):
+    return [decrypt_text(text, password, fernet_thing) for text in text_list]
