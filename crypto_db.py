1dae57b836aedd7b10f3d50c5c0a7da527fa7570
Heikki Jylhä <hjjylh@gmail.com>
2021-07-02 21:34:38 +0300

    reworked crypto_db.py to not use apps table, and added function to check if info is in database

--- crypto_db.py
+++ crypto_db.py
@@ -7,16 +7,10 @@ def add_master_table(username, master_pw, fernet_thing):
     un_encrypted = crypto_stuff.encrypt_text(username, master_pw, fernet_thing)
     db.add_to_master_table(un_encrypted, pw_encrypted)
 
-# TODO: this needs to check that there is no duplicates
+# this does not check if we add duplicates
 def add_info(info, master_pw, fernet_thing):
     info_encrypted = crypto_stuff.encrypt_text_list(info, master_pw, fernet_thing)
     db.add_to_info_table(info_encrypted)
-    app_list = get_app_list(master_pw, fernet_thing)
-    # let's not worry about lower/uppercase
-    app_list_l = [app.lower() for app in app_list]
-    # app_name is info[3]
-    if not(info[3].lower() in app_list_l):
-        db.add_to_apps_table(info_encrypted[3])
 
 
 # get from database and decrypt
@@ -25,7 +19,18 @@ def get_master_table(master_pw, fernet_thing):
     master = crypto_stuff.decrypt_text_list(master_e, master_pw, fernet_thing)
     return master
 
-def get_info_for_app(app_name, master_pw, fernet_thing):
+def get_info_decrypted_w_rowid(master_pw, fernet_thing):
+    results_e = db.get_info_w_rowid()
+    results = []
+    for result in results_e:
+        result_d = crypto_stuff.decrypt_text_list(result[1:], master_pw, fernet_thing)
+        # insert rowid to the start
+        result_d.insert(0, result[0])
+        results.append(result_d)
+    return results
+
+# find app_name from database (match ignoring capitalization)
+def get_info_for_exact_app(app_name, master_pw, fernet_thing):
     results_e = db.get_info()
     results = []
     for result in results_e:
@@ -34,6 +39,18 @@ def get_info_for_app(app_name, master_pw, fernet_thing):
             results.append(result_d)
     return results
 
+# find app_name from database (apps whose name contains app_name)
+# info = (username, email, pw, app, url)
+def get_info_for_app(app_name, master_pw, fernet_thing):
+    results_e = db.get_info()
+    results = []
+    for result in results_e:
+        result_d = crypto_stuff.decrypt_text_list(result, master_pw, fernet_thing)
+        if result_d[3].lower().find(app_name.lower()) > -1:
+            results.append(result_d)
+    return results
+
+# same as above, but with rowid included
 # info = (rowid, username, email, pw, app, url)
 def get_info_for_app_w_rowid(app_name, master_pw, fernet_thing):
     results_e = db.get_info_w_rowid()
@@ -41,25 +58,39 @@ def get_info_for_app_w_rowid(app_name, master_pw, fernet_thing):
     for result in results_e:
         # rowid is not encrypted, so ignore result[0]
         result_d = crypto_stuff.decrypt_text_list(result[1:], master_pw, fernet_thing)
-        if result_d[3].lower() == app_name.lower():
+        if result_d[3].lower().find(app_name.lower()) > -1:
             # add rowid back
-            r = [result[0]]
-            for item in result_d:
-                r.append(item)
-            results.append(r)
-            # testing
-            print(r)
+            result_d.insert(0, result[0])
+            results.append(result_d)
     return results
 
+# create a list of apps in database
 def get_app_list(master_pw, fernet_thing):
-    app_list_e = db.get_app_list()
-    app_list = crypto_stuff.decrypt_text_list(app_list_e, master_pw, fernet_thing)
+    app_list = []
+    data = get_info_decrypted_w_rowid(master_pw, fernet_thing)
+    for row in data:
+        is_new = True
+        # check for repeats
+        for app in app_list:
+            if app.lower() == row[4].lower():
+                is_new = False
+                break
+        if is_new:
+            app_list.append(row[4])
     return app_list
-    # for app in app_list:
-    #     print("\t", app)
 
 # change the password stored in row rowid
 def change_pw_in_row(rowid, new_pw, master_pw, fernet_thing):
-    # print(rowid)
     new_pw_e = crypto_stuff.encrypt_text(new_pw, master_pw, fernet_thing)
     db.modify_password_by_rowid(rowid, new_pw_e)
+
+# check if app name and username are in database
+# info = (username, email, app, url)
+def is_info_in_db(info, master_pw, fernet_thing):
+    possible_rows = get_info_for_exact_app(info[2], master_pw, fernet_thing)
+    if possible_rows == []:
+        return False
+    for row in possible_rows:
+        if row[0] == info[0]:
+            return True
+    return False
