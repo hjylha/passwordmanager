b0981c3e41d8a14129a8ad590205af430ec7abc0
Heikki Jylhä <hjjylh@gmail.com>
2021-11-30 16:55:49 +0200

    added hashing with argon2 and password generator to crypto_stuff.py

--- crypto_stuff.py
+++ crypto_stuff.py
@@ -1,8 +1,12 @@
+import string
 import base64
 import hashlib
+import secrets
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import argon2
+from argon2 import PasswordHasher
 
 # create the encryptor/decryptor thingie through password and salt
 def do_crypto_stuff(password, given_salt, num_of_iterations):
@@ -27,6 +31,7 @@ def decrypt_text_list(text_list, fernet_thing):
     return [decrypt_text(text, fernet_thing) for text in text_list]
 
 # hashing passwords (and usernames?)
+# the old way
 def hash_text(text, salt, num_of_iterations=200_000):
     encoded_text = text.encode('utf-8')
     iteration = num_of_iterations + len(text) * 1000
@@ -36,3 +41,45 @@ def create_hash_storage(text, salt, num_of_iterations=200_000):
     key = hash_text(text, salt, num_of_iterations)
     stored_value = salt + key
     return stored_value
+
+# the new way (with argon2)
+def hash_password(password):
+    ph = PasswordHasher()
+    return ph.hash(password)
+
+# check if hashed password = hash
+def check_password_hash(hash, password):
+    ph = PasswordHasher()
+    try:
+        if ph.verify(hash, password):
+            return True
+    except argon2.exceptions.VerifyMismatchError:
+        return False
+
+# hash a couple of values
+def hash_tuple(a_tuple):
+    ph = PasswordHasher()
+    return tuple(ph.hash(item) for item in a_tuple)
+
+def check_tuple(h_tuple, a_tuple):
+    ph = PasswordHasher()
+    for hash, text in zip(h_tuple, a_tuple):
+        try:
+            ph.verify(hash, text)
+        except argon2.exceptions.VerifyMismatchError:
+            return False
+    return True
+
+
+# get string of characters for password generator (0: all, 1: only letters and numbers)
+def get_characters(mode=0):
+    characters = string.ascii_letters + string.digits
+    if not mode:
+        extra_characters = '(,._-*~"<>/|!@#$%^&)+='
+        return characters + extra_characters
+    return characters
+
+# generate password of given length with or without extra characters
+def generate_password(length=20, choice=0):
+    characters = get_characters(choice)
+    return "".join(secrets.choice(characters) for _ in range(length))
--- tests/test_crypto_stuff.py
+++ tests/test_crypto_stuff.py
@@ -20,6 +20,7 @@ def salt():
 def iteration_num():
     return 157
 
+# this is tested immediately below
 @pytest.fixture
 def fernet_thing(pw, salt):
     return cs.do_crypto_stuff(pw, salt, 37)
@@ -45,16 +46,79 @@ def test_decrypt_text(fernet_thing):
     assert text == cs.decrypt_text(encrypted, fernet_thing)
 
 
-def test_encrypt_text_list():
-    pass
+def test_encrypt_text_list(fernet_thing):
+    text_list = ["first", "second", "third"]
+    encryptions = [text_list]
+    for _ in range(5):
+        encryption = cs.encrypt_text_list(text_list, fernet_thing)
+        assert len(encryption) == len(text_list)
+        for i, e_text in enumerate(encryption):
+            assert e_text not in [item[i] for item in encryptions]
 
-def test_decrypt_text_list():
-    pass
+def test_decrypt_text_list(fernet_thing):
+    text_list = ["first", "second", "third"]
+    e_text_list = cs.encrypt_text_list(text_list, fernet_thing)
+    assert text_list == cs.decrypt_text_list(e_text_list, fernet_thing)
 
 
-def test_hash_text():
-    pass
+def test_hash_text(salt, iteration_num):
+    text = "all good"
+    hashed_text = cs.hash_text(text, salt, iteration_num)
+    assert isinstance(hashed_text, bytes)
+    assert len(hashed_text) == 32
 
 
-def test_create_hash_storage():
-    pass
+def test_create_hash_storage(salt, iteration_num):
+    text = "all good"
+    hash_storage = cs.create_hash_storage(text, salt, iteration_num)
+    assert len(hash_storage) == 48
+    assert hash_storage == salt + cs.hash_text(text, salt, iteration_num)
+
+
+def test_hash_password():
+    pwd = "s3cur3 p455w0rd"
+    assert isinstance(cs.hash_password(pwd), str)
+
+def test_check_password_hash():
+    pwd = "s3cur3 p455w0rd"
+    hash = cs.hash_password(pwd)
+    assert cs.check_password_hash(hash, pwd)
+    assert not cs.check_password_hash(hash, "not right")
+
+
+def test_hash_tuple():
+    something = ("first", "second", "third")
+    hashed_tuple = cs.hash_tuple(something)
+    assert len(hashed_tuple) == len(something)
+    for item in hashed_tuple:
+        assert isinstance(item, str)
+
+def test_check_tuple():
+    something = ("first", "second", "third")
+    hashed_tuple = cs.hash_tuple(something)
+    assert cs.check_tuple(hashed_tuple, something)
+
+    something_else = ("a", "second", "third")
+    assert not cs.check_tuple(hashed_tuple, something_else)
+
+
+def test_get_characters():
+    all_chars = cs.get_characters()
+    some_chars = cs.get_characters(1)
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    numbers = "0123456789"
+    extra_chars = '(,._-*~"<>/|!@#$%^&)+='
+    assert letters in all_chars
+    assert letters in some_chars
+    assert letters.upper() in all_chars
+    assert letters.upper() in some_chars
+    assert numbers in all_chars
+    assert numbers in some_chars
+    assert extra_chars in all_chars
+    for char in extra_chars:
+        assert char not in some_chars
+
+
+def test_generate_password():
+    pwd = cs.generate_password()
+    assert len(pwd) == 20
