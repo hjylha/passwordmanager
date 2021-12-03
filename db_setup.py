5dc732109cd9b6277b9d386e8947fe1747904665
Heikki Jylhä <hjjylh@gmail.com>
2021-12-03 22:52:26 +0200

    collected table data for all dbs in one dict

--- db_setup.py
+++ db_setup.py
@@ -1,31 +1,39 @@
 '''names of tables and columns'''
 
-# the keys db
-keys_table_data = dict()
-keys_table_data['auth'] = {'hash': ('TEXT', 'NOT NULL'),
+table_data = dict()
+type_tuples = dict()
+
+# the auth db
+table_data['auth'] = {'auth': {'hash': ('TEXT', 'NOT NULL'),
                             'master_key': ('TEXT', 'NOT NULL'),
-                            'date_modified': ('TEXT', 'NOT NULL')}
-keys_table_data['app_keys'] = {'key': ('TEXT', 'NOT NULL'),
+                            'date_modified': ('TEXT', 'NOT NULL')}}
+type_tuples['auth'] = ('hash', 'key', 'normal')
+
+# the keys db
+table_data['keys'] = dict()
+table_data['keys']['app_keys'] = {'key': ('TEXT', 'NOT NULL'),
                                 'in_use': ('TEXT', 'NOT NULL'),
                                 'date_modified': ('TEXT', 'NOT NULL')}
-keys_table_data['email_keys'] = {'key': ('TEXT', 'NOT NULL'),
+table_data['keys']['email_keys'] = {'key': ('TEXT', 'NOT NULL'),
                                 'in_use': ('TEXT', 'NOT NULL'),
                                 'date_modified': ('TEXT', 'NOT NULL')}
-keys_table_data['data_keys'] = {'key': ('TEXT', 'NOT NULL'),
+table_data['keys']['data_keys'] = {'key': ('TEXT', 'NOT NULL'),
                                 'in_use': ('TEXT', 'NOT NULL'),
                                 'date_modified': ('TEXT', 'NOT NULL')}
+type_tuples['keys'] = ('key', 'normal', 'normal')
 
 # the actual data db
-data_db_table_data = dict()
-data_db_table_data['apps'] = {'app': ('TEXT', 'NOT NULL'),
+table_data['password'] = dict()
+table_data['password']['apps'] = {'app': ('TEXT', 'NOT NULL'),
                                 'num_of_instances': ('TEXT', 'NOT NULL'),
                                 'date_modified': ('TEXT', 'NOT NULL')}
-data_db_table_data['emails'] = {'email': ('TEXT', 'NOT NULL'),
+table_data['password']['emails'] = {'email': ('TEXT', 'NOT NULL'),
                                 'num_of_instances': ('TEXT', 'NOT NULL'),
                                 'date_modified': ('TEXT', 'NOT NULL')}
-data_db_table_data['data'] = {'username': ('TEXT', 'NOT NULL'),
+table_data['password']['data'] = {'username': ('TEXT', 'NOT NULL'),
                                 'email': ('TEXT', 'NOT NULL'),
                                 'password': ('TEXT', 'NOT NULL'),
                                 'app_name': ('TEXT', 'NOT NULL'),
                                 'url': ('TEXT', 'NOT NULL'),
                                 'date_modified': ('TEXT', 'NOT NULL')}
+type_tuples['password'] = tuple('normal' for _ in range(6))
