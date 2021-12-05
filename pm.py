dea0ced3e69229156314e802da372736f9c68627
Heikki Jylhä <hjjylh@gmail.com>
2021-12-05 22:58:06 +0200

    reformatted some text

--- pm.py
+++ pm.py
@@ -14,22 +14,22 @@ salt_filename = default_salt_filename
 
 # ask to press enter, and clear clipboard
 def end_prompt():
-    print("\n")
-    getpass.getpass("Press ENTER to return back to menu. (This also clears clipboard)")
+    print('\n')
+    getpass.getpass('Press ENTER to return back to menu. (This also clears clipboard)')
     pm.clear_clipboard()
-    print("\n")
+    print('\n')
 
 
 def password_manager(db_filename=default_db_filename, salt_filename=default_salt_filename):
     # starting text
-    print("Welcome to my Password Manager\n")
+    print('Welcome to my Password Manager\n')
 
     # check if DB has been initiated
     # is_initiated = check_db(db_filename)
     if check_db(db_filename):
         # if initiated, ask for username and password
         username, master_password = pm.ask_for_username_and_password()
-        # username = input("Enter your username: ")
+        # username = input('Enter your username: ')
         # master_password = getpass.getpass(prompt='Enter the master password: ')
         f = do_crypto_stuff(master_password, load_salt(salt_filename), 100000)
         # check that they are in the database
@@ -39,14 +39,14 @@ def password_manager(db_filename=default_db_filename, salt_filename=default_salt
         pw_h = create_hash_storage(master_password, load_salt(salt_filename))
         # if not(username == master_d[0] and master_password == master_d[1]):
         if not(un_h == master_h[0] and pw_h == master_h[1]):
-            print("Invalid password or username")
+            print('Invalid password or username')
             exit()
     else:
-        print("Password database has not been initiated yet.")
-        # print("Do you want to initiate it? (Y/N)")
-        answer = pm.yes_or_no_question("Do you want to initiate it?")
-        if answer.lower() == "y":
-            print("\n")
+        print('Password database has not been initiated yet.')
+        # print('Do you want to initiate it? (Y/N)')
+        answer = pm.yes_or_no_question('Do you want to initiate it?')
+        if answer.lower() == 'y':
+            print('\n')
             initiation_data = initiate_db()
             username = initiation_data[0]
             master_password = initiation_data[1]
@@ -57,41 +57,41 @@ def password_manager(db_filename=default_db_filename, salt_filename=default_salt
     # maybe clear screen before starting
     pm.clear_screen()
     master_password = None
-    print("Password Manager for user", username, "\n")
+    print(f'Password Manager for user {username}\n')
 
     # the program loop
     while True:
-        print("What do you want to do?")
-        print("(1) Add a new password")
-        print("(2) Change a password")
-        print("(3) Retrieve a password")
-        print("(4) Delete a password")
-        print("(5) Show a list of apps with passwords")
-        print("(0) Exit")
+        print('What do you want to do?')
+        print('(1) Add a new password')
+        print('(2) Change a password')
+        print('(3) Retrieve a password')
+        print('(4) Delete a password')
+        print('(5) Show a list of apps with passwords')
+        print('(0) Exit')
         action = input()
-        if action == "0":
+        if action == '0':
             exit()
-        elif action == "1":
+        elif action == '1':
             pm.add_password(f)
             end_prompt()
-        elif action == "2":
+        elif action == '2':
             pm.change_password(f)
             end_prompt()
-        elif action == "3":
+        elif action == '3':
             pm.find_password_for_app(f)
             end_prompt()
-        elif action == "4":
+        elif action == '4':
             pm.delete_password(f)
             end_prompt()
-        elif action == "5":
-            print("These are the apps you have saved passwords for:")
+        elif action == '5':
+            print('These are the apps you have saved passwords for:')
             app_list = crypto_db.get_app_list(f)
             for app in app_list:
-                print("\t", app)
-            print("\n")
+                print('\t', app)
+            print('\n')
             end_prompt()
         else:
-            print("Invalid input. Please choose one of the options.\n")
+            print('Invalid input. Please choose one of the options.\n')
 
 if __name__ == '__main__':
     password_manager()
--- pm_fcns.py
+++ pm_fcns.py
@@ -17,88 +17,88 @@ def clear_screen():
 
 # is it enough to just copy something to the clipboard, insted of clearing it?
 def clear_clipboard():
-    pyperclip.copy("nothing here")
+    pyperclip.copy('nothing here')
     # in windows:
     # https://stackoverflow.com/questions/53226110/how-to-clear-clipboard-in-using-python/53226144
     # from ctypes import windll
     # if windll.user32.OpenClipboard(None):
     #     windll.user32.EmptyClipboard()
     #     windll.user32.CloseClipboard()
-    # print("Clipboard cleared")
+    # print('Clipboard cleared')
 
 # ask for username and password
 def ask_for_username_and_password():
     # print()
-    username = getpass.getpass("Enter your username: ")
-    password = getpass.getpass("Enter the master password: ")
+    username = getpass.getpass('Enter your username: ')
+    password = getpass.getpass('Enter the master password: ')
     return (username, password)
 
 # print a header line and a line from database
 def print_info(first_line, info):
     print(first_line)
-    print("username:", info[0])
-    print("password:", "[HIDDEN]")
-    print("email:", info[1])
-    print("app name:", info[3])
-    print("app url:", info[4])
+    print('username:', info[0])
+    print('password:', '[HIDDEN]')
+    print('email:', info[1])
+    print('app name:', info[3])
+    print('app url:', info[4])
 
 # get the answer to a yes/no question from user
 def yes_or_no_question(question_text):
-    help_text = "Please answer Y or N for yes or no."
+    help_text = 'Please answer Y or N for yes or no.'
     while True:
-        answer = input(question_text + " (Y/N) ")
-        if answer.lower() == "y":
-            return "y"
-        if answer.lower() == "n":
-            return "n"
+        answer = input(f'{question_text} (Y/N) ')
+        if answer.lower() == 'y':
+            return 'y'
+        if answer.lower() == 'n':
+            return 'n'
         print(help_text)
 
 # info = [username, email, password, app, url]
 def obtain_password(info, reveal_pw):
-    # print("Password found for", info[4], "user", info[1])
-    print("\nPassword found.")
-    print("App:", info[4])
-    print("Username:", info[1])
+    # print('Password found for', info[4], 'user', info[1])
+    print('\nPassword found.')
+    print('App:', info[4])
+    print('Username:', info[1])
     if reveal_pw:
         reveal_password(info[3])
-    # input("Press ENTER to continue")
+    # input('Press ENTER to continue')
 
 def reveal_password(pw):
-    print("\nWhat do you want to do:")
-    print("(1) Copy the password to clipboard")
-    print("(2) Show the password")
-    print("(0) Skip password reveal")
+    print('\nWhat do you want to do:')
+    print('(1) Copy the password to clipboard')
+    print('(2) Show the password')
+    print('(0) Skip password reveal')
     while True:
         pick = input()
-        if pick == "0":
+        if pick == '0':
             return
-        if pick == "1":
+        if pick == '1':
             pyperclip.copy(pw)
-            print("Password has been copied to the clipboard.")
+            print('Password has been copied to the clipboard.')
             return
-        if pick == "2":
-            print("Password is:", pw)
+        if pick == '2':
+            print(f'Password is: {pw}')
             return
 
 
 # choose how to generate password
 def how_to_generate_pw():
-    print("Do you want to")
-    print("(1) Generate a password using letters, numbers and other characters")
-    print("(2) Generate a password using only letters and numbers")
-    print("(3) Come up with the password on my own")
-    print("(0) Cancel")
+    print('Do you want to')
+    print('(1) Generate a password using letters, numbers and other characters')
+    print('(2) Generate a password using only letters and numbers')
+    print('(3) Come up with the password on my own')
+    print('(0) Cancel')
     while True:
         sel = input()
-        if sel in ("0", "1", "2", "3"):
+        if sel in ('0', '1', '2', '3'):
             return int(sel)
-        print("Please choose one of the options.")
+        print('Please choose one of the options.')
 
 # generate password (type_num = 1 or 2) or let the user come up with one (type_num = 3)
 def generate_pw(type_num):
     # add the cancel password option just to be sure
     if type_num == 0:
-        return ""
+        return ''
     # generate using all characters
     if type_num == 1:
         return generate_password()
@@ -108,30 +108,30 @@ def generate_pw(type_num):
     # user makes the password (or saves an existing one to database)
     if type_num == 3:
         while True:
-            pw = getpass.getpass("Type the password you want: ")
-            pw2 = getpass.getpass("Type the password again: ")
+            pw = getpass.getpass('Type the password you want: ')
+            pw2 = getpass.getpass('Type the password again: ')
             if pw == pw2:
-                if pw != "":
+                if pw != '':
                     return pw
-                ans = yes_or_no_question("you did not type any password. Do you want to try again?")
-                if ans == "n":
-                    return ""
+                ans = yes_or_no_question('you did not type any password. Do you want to try again?')
+                if ans == 'n':
+                    return ''
             else:
-                ans = yes_or_no_question("Passwords were not the same. Do you want to try again?")
-                if ans == "n":
-                    return ""
+                ans = yes_or_no_question('Passwords were not the same. Do you want to try again?')
+                if ans == 'n':
+                    return ''
 
 
 # get app name, url of app, username and email associated with app from user
 def get_info_from_user():
-    print("Write the name of the app you want to add password to:")
-    app_name = input("\t")
-    print("What is the internet address of this app?")
-    url = input("\t")
-    print("What is your username associated with this app?")
-    un = input("\t")
-    print("What is your email associated with this app?")
-    email = input("\t")
+    print('Write the name of the app you want to add password to:')
+    app_name = input('\t')
+    print('What is the internet address of this app?')
+    url = input('\t')
+    print('What is your username associated with this app?')
+    un = input('\t')
+    print('What is your email associated with this app?')
+    email = input('\t')
     return (un, email, app_name, url)
 
 # get the info and check that app and username are not already in database
@@ -141,38 +141,38 @@ def get_unique_info_from_user(fernet_thing):
         info = get_info_from_user()
         if not(crypto_db.is_info_in_db(info, fernet_thing)):
             return info
-        print("Password for user", info[0], "to app", info[2], "already in database.")
-        ans = yes_or_no_question("Do you want to try again?")
-        if ans == "n":
+        print(f'Password for user {info[0]} to app {info[2]} already in database.')
+        ans = yes_or_no_question('Do you want to try again?')
+        if ans == 'n':
             # return empty list if user does not want to try again
             return []
 
 # find correct app and username from users input
 def find_app_and_username(first_line, fernet_thing):
     print(first_line)
-    app_name = input("\t")
+    app_name = input('\t')
     results = crypto_db.get_info_for_app_w_rowid(app_name, fernet_thing)
     if results == []:
-        print("No passwords related to", app_name, "found\n")
+        print(f'No passwords related to {app_name} found\n')
         return []
     if len(results) == 1:
         return results[0]
     # app_name might not be the actual name
-    print("More than one password related to", app_name, "in the database.")
+    print(f'More than one password related to {app_name} in the database.')
     # selected = False
     while True:
-        print("Select the correct app and username:")
+        print('Select the correct app and username:')
         for i in range(len(results)):
-            print("(" + str(i+1) + ")", "App:", results[i][4])
-            # blank_list = [" " for _ in range(len(str(i+1)) + 2)]
-            blank = "".join([" " for _ in range(len(str(i+1)) + 2)])
-            print(blank, "username:", results[i][1])
-        print("(0) Cancel search")
+            print(f'({str(i+1)}) App: {results[i][4]}')
+            # blank_list = [' ' for _ in range(len(str(i+1)) + 2)]
+            blank = ''.join([' ' for _ in range(len(str(i+1)) + 2)])
+            print(blank, f'{blank} username: {results[i][1]}')
+        print('(0) Cancel search')
         selection = input()
         try:
             index = int(selection) - 1
             if index == -1:
-                print("Search cancelled.")
+                print('Search cancelled.')
                 return []
             try:
                 result = results[index]
@@ -184,7 +184,7 @@ def find_app_and_username(first_line, fernet_thing):
 
 # find and reveal password based on users input
 def find_password_for_app(fernet_thing):
-    result = find_app_and_username("Write the name of the app you want password for:", fernet_thing)
+    result = find_app_and_username('Write the name of the app you want password for:', fernet_thing)
     if result == []:
         return
     obtain_password(result, True)
@@ -192,48 +192,48 @@ def find_password_for_app(fernet_thing):
 
 # try to add info into database. returns True if process is complete (added or cancelled)
 def save_password_to_db_or_not(info, fernet_thing):
-    answer = yes_or_no_question("Do you want to save this password to the database?")
-    if answer == "y":
-        print_info("The following information will be saved to the database:", info)
-        answer2 = yes_or_no_question("Do you want to proceed?")
-        if answer2 == "y":
+    answer = yes_or_no_question('Do you want to save this password to the database?')
+    if answer == 'y':
+        print_info('The following information will be saved to the database:', info)
+        answer2 = yes_or_no_question('Do you want to proceed?')
+        if answer2 == 'y':
             crypto_db.add_info(info, fernet_thing)
-            print("Password for user " + info[0] + " to app " + info[3] + " has been saved to the database.")
+            print(f'Password for user {info[0]} to app {info[3]} has been saved to the database.')
             return True
-        if answer2 == "n":
-            answer3 = yes_or_no_question("Do you want to generate password again?")
-            if answer3 == "y":
+        if answer2 == 'n':
+            answer3 = yes_or_no_question('Do you want to generate password again?')
+            if answer3 == 'y':
                 return False
-            if answer3 == "n":
-                print("Adding password canceled.")
+            if answer3 == 'n':
+                print('Adding password canceled.')
                 return True
-    if answer == "n":
-        answer2 = yes_or_no_question("Do you want to generate password again?")
-        if answer2 == "y":
+    if answer == 'n':
+        answer2 = yes_or_no_question('Do you want to generate password again?')
+        if answer2 == 'y':
             return False
-        if answer2 == "n":
-            print("Adding password canceled.")
+        if answer2 == 'n':
+            print('Adding password canceled.')
             return True
 
 # db_line has (rowid, username, email, password, app, url)
 def change_password_in_db_or_not(db_line, fernet_thing):
-    answer = yes_or_no_question("Do you want to save this password to the database?")
-    if answer == "y":
-        print_info("The following information will be updated in the database:", db_line[1:])
-        answer2 = yes_or_no_question("Do you want to proceed?")
-        if answer2 == "y":
+    answer = yes_or_no_question('Do you want to save this password to the database?')
+    if answer == 'y':
+        print_info('The following information will be updated in the database:', db_line[1:])
+        answer2 = yes_or_no_question('Do you want to proceed?')
+        if answer2 == 'y':
             crypto_db.change_pw_in_row(db_line[0], db_line[3], fernet_thing)
-            print("Password has been updated.")
+            print('Password has been updated.')
             return True
-        if answer2 == "n":
-            print("Changing password canceled.")
+        if answer2 == 'n':
+            print('Changing password canceled.')
             return True
-    if answer == "n":
-        answer2 = yes_or_no_question("Do you want to generate new password again?")
-        if answer2 == "y":
+    if answer == 'n':
+        answer2 = yes_or_no_question('Do you want to generate new password again?')
+        if answer2 == 'y':
             return False
-        if answer2 == "n":
-            print("Changing password canceled.")
+        if answer2 == 'n':
+            print('Changing password canceled.')
             return True
 
 
@@ -243,7 +243,7 @@ def add_password(fernet_thing):
     info = get_unique_info_from_user(fernet_thing)
     # if user does not want to give info, back to the main menu
     if info == []:
-        print("Adding password canceled.")
+        print('Adding password canceled.')
         return
     # while loop, since user might want to generate password again
     while True:
@@ -251,12 +251,12 @@ def add_password(fernet_thing):
         gen_type = how_to_generate_pw()
         pw = generate_pw(gen_type)
         # if no password, cancel the process
-        if pw == "":
-            print("Adding password canceled.")
+        if pw == '':
+            print('Adding password canceled.')
             return
         # user might want to check the generated password
         if gen_type in (1, 2):
-            print("Password has been generated.")
+            print('Password has been generated.')
             reveal_password(pw)
         # finally, try to save the password
         db_line = (info[0], info[1], pw, info[2], info[3])
@@ -270,12 +270,12 @@ def change_password_in_db(db_line, fernet_thing):
         gen_type = how_to_generate_pw()
         pw = generate_pw(gen_type)
         # if no password, cancel the process
-        if pw == "":
-            print("Changing password canceled.")
+        if pw == '':
+            print('Changing password canceled.')
             return
         # user might want to check the generated password
         if gen_type in (1, 2):
-            print("Password has been generated.")
+            print('Password has been generated.')
             reveal_password(pw)
         # try to update database
         db_line_new = (db_line[0], db_line[1], db_line[2], pw, db_line[4], db_line[5])
@@ -285,7 +285,7 @@ def change_password_in_db(db_line, fernet_thing):
 
 # the whole process of changing a password
 def change_password(fernet_thing):
-    result = find_app_and_username("What is the app you want to change password for?", fernet_thing)
+    result = find_app_and_username('What is the app you want to change password for?', fernet_thing)
     # if nothing found, return to main menu
     if result == []:
         return
@@ -294,13 +294,13 @@ def change_password(fernet_thing):
     change_password_in_db(result, fernet_thing)
 
 def delete_password(fernet_thing):
-    result = find_app_and_username("What is the app whose password you want to delete", fernet_thing)
+    result = find_app_and_username('What is the app whose password you want to delete', fernet_thing)
     if result == []:
         return
-    print_info("The following information will be deleted from the database:", result[1:])
-    ans = yes_or_no_question("Do you want to delete this information?")
-    if ans == "y":
+    print_info('The following information will be deleted from the database:', result[1:])
+    ans = yes_or_no_question('Do you want to delete this information?')
+    if ans == 'y':
         delete_row(result[0])
-        print("Password has been deleted from the database.")
-    if ans == "n":
-        print("Delete process cancelled.")
+        print('Password has been deleted from the database.')
+    if ans == 'n':
+        print('Delete process cancelled.')
