22304efdde45778b8cfd19f0a87be0706230f414
Heikki Jylhä <hjjylh@gmail.com>
2021-12-06 17:24:50 +0200

    added DB_auth class with some fcns

--- dbs.py
+++ dbs.py
@@ -11,12 +11,12 @@ import crypto_stuff as cs
 def generate_dummy_string(type_of_string, fernet_obj=None, password_hasher=None):
     if type_of_string == 'normal':
         if fernet_obj is None:
-            fernet_obj = cs.Fernet(cs.Fernet.generate_key())
+            fernet_obj = Fernet(Fernet.generate_key())
         return cs.encrypt_text(cs.generate_password(), fernet_obj)
     elif type_of_string == 'key':
         if fernet_obj is None:
-            fernet_obj = cs.Fernet(cs.Fernet.generate_key())
-        return fernet_obj.encrypt(cs.Fernet.generate_key())
+            fernet_obj = Fernet(Fernet.generate_key())
+        return fernet_obj.encrypt(Fernet.generate_key())
     elif type_of_string == 'hash':
         if password_hasher is None:
             password_hasher = cs.PasswordHasher()
@@ -59,6 +59,87 @@ def initiate_db(filepath, db_type, num_of_dummy_inserts=10):
     return db
     
 
+class DB_auth(DB_general):
+
+    def __init__(self, filepath_of_db) -> None:
+        super().__init__(filepath_of_db)
+        self.table = 'auth'
+        self.cols = ('hash', 'master_key', 'date_modified')
+        self.length = len(self.select_all(self.table))
+        # check that it contains the required stuff
+        # TODO: maybe improve these
+        if table_data[self.table][self.table] != self.tables[self.table]:
+            raise Exception('No auth table in the database.')
+        if len(self.select_all(self.table)[0]) != 4:
+            raise Exception('Something is wrong with columns in auth table')
+
+    def check_uniqueness_of_keys(self):
+        keys = tuple(row[0] for row in self.select_columns(self.table, self.cols[1:]))
+        if len(keys) == len(set(keys)):
+            return True
+        # if len(keys) != len(set(keys)):
+        return False
+
+    def is_key_in_keys(self, key):
+        keys = tuple(row[0] for row in self.select_columns(self.table, self.cols[1:]))
+        if key in keys:
+            return True
+        return False
+
+    # check the given password and return the corresponding (encrypted) master key
+    def check_password(self, password):
+        content = self.select_columns(self.table, self.cols[:2])
+        ph = cs.PasswordHasher()
+        search_results = []
+        for row in content:
+            try:
+                ph.verify(row[0], password)
+                search_results.append(row[1])
+            except cs.argon2.exceptions.VerifyMismatchError:
+                pass
+        if len(search_results) == 1:
+            return search_results[0]
+        # not sure what to do if password is in db multiple times
+        if len(search_results) > 1:
+            raise Exception('This password is in the database more than once.')
+        if not search_results:
+            raise Exception('Given password not in database.')
+        
+    # add password and hash it; generate key and time, and encrypt them
+    def add_password(self, password, salt):
+        # from pm_data import salt_thingie
+        ph = cs.PasswordHasher()
+        hash = ph.hash(password)
+        f = cs.do_crypto_stuff(password, salt, 200_000)
+        while True:
+            master_key = Fernet.generate_key()
+            encrypted_master_key = f.encrypt(master_key)
+            if not self.is_key_in_keys(encrypted_master_key):
+                encrypted_time = cs.encrypt_text(str(int(time.time())), f)
+                datarow = (hash, encrypted_master_key, encrypted_time)
+                rowid = cs.secrets.randbelow(self.length) + 1
+                self.update_by_rowid(self.table, self.cols, datarow, rowid)
+                break
+    
+    # get the actual master key
+    def decrypt_key(self, encrypted_key, password, salt):
+        f = cs.do_crypto_stuff(password, salt, 200_000)
+        return f.decrypt(encrypted_key)
+
+
+class DB_keys(DB_general):
+    
+    def find_vacancy(self, table):
+        pass
+
+    def insert_key(self, key, table, rowid):
+        pass
+
+
+class DB_password(DB_general):
+
+    def insert_data(self, data, fernet):
+        pass
 
 
 
--- tests/test_dbs.py
+++ tests/test_dbs.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import string
 import os
+from cryptography.fernet import Fernet
 
 import pytest
 
 import fix_imports
 import dbs
+from dbs import DB_auth
 
 
 # 'empty' db
@@ -14,6 +16,11 @@ def db():
     db_path = Path('test_db.db')
     yield dbs.DB_general(db_path)
     db_path.unlink()
+
+# test auth db
+@pytest.fixture
+def dba():
+    return DB_auth('test_db_auth.db')
 
 # test key db
 @pytest.fixture
@@ -25,6 +32,7 @@ def dbk():
 def dbp():
     return dbs.initiate_password_db('test_pwd.db')
 
+
 class TestDummydata():
 
     def test_generate_dummy_string(self):
@@ -35,6 +43,8 @@ class TestDummydata():
         string_type = 'key'
         dummy_string = dbs.generate_dummy_string(string_type)
         assert isinstance(dummy_string, bytes)
+        # f = Fernet(Fernet.generate_key())
+        # f.decrypt(dummy_string)
 
         string_type = 'hash'
         dummy_string = dbs.generate_dummy_string(string_type)
@@ -43,6 +53,10 @@ class TestDummydata():
         assert len(dummy_list) == 6
         assert dummy_list[0] == ''
         assert dummy_list[1] == 'argon2id'
+
+        string_type = 'not a type'
+        with pytest.raises(Exception):
+            dbs.generate_dummy_string(string_type)
 
         # how about with given encryptor or hasher?
         f = dbs.cs.do_crypto_stuff('password', os.urandom(16), 5)
@@ -55,6 +69,8 @@ class TestDummydata():
         string_type = 'key'
         dummy_string = dbs.generate_dummy_string(string_type, f)
         assert isinstance(dummy_string, bytes)
+        # try to deccrypt with the same f should not be a problem
+        f.decrypt(dummy_string)
 
         string_type = 'hash'
         dummy_string = dbs.generate_dummy_string(string_type, password_hasher=ph)
@@ -85,9 +101,70 @@ class TestInitiation():
         assert len(dbk.select_all('email_keys')) == 10
         assert len(dbk.select_all('data_keys')) == 10
 
-
     def test_initiate_password_db(self, db):
         dbp = dbs.initiate_db(db.filepath, 'password')
         assert len(dbp.select_all('apps')) == 10
         assert len(dbp.select_all('emails')) == 10
         assert len(dbp.select_all('data')) == 10
+    
+    def test_initiate_db_fail(self, db):
+        with pytest.raises(Exception):
+            dbs.initiate_db(db.filepath, 'not a type')
+
+class TestDBauth():
+
+    def test_is_key_in_keys(self, dba):
+        key = dbs.Fernet.generate_key()
+        e_key = dbs.Fernet(key).encrypt(key)
+        # it is extremely unlikely that e_key is among keys
+        assert not dba.is_key_in_keys(e_key)
+
+        # this should be in keys
+        e_key = b'gAAAAABhrgu_1yd1dxYsD7DNk36vqwbmzwF6zAaBMbbD2Oyh5NDGGPUBvZ37fOQjH_ZhfOLGV-tTUkbNSj2wKBsjdKbctheo1UeiGvFDLRdN7yFrRs_rre08WU4XF0Pky76ZZ8OkN6b_'
+        assert dba.is_key_in_keys(e_key)
+
+    def test_check_uniqueness_of_keys(self, dba):
+        # it is extremely unlikely that there would be two same encrypted keys
+        assert dba.check_uniqueness_of_keys()
+
+    # test add and check password
+    def test_add_password(self, dba):
+        data0 = dba.select_all(dba.table)
+        password = dbs.cs.generate_password()
+        salt = b'SBHe5zI8OkN94rsVHSZE3dwAvimz-ukl11ONcJgEAbo='
+        dba.add_password(password, salt)
+
+        # something should have changed
+        data = dba.select_all(dba.table)
+        assert any(row0 != row for row0, row in zip(data0, data))
+
+        # this key should be in keys
+        e_key = dba.check_password(password)
+        assert dba.is_key_in_keys(e_key)
+
+    # def test_check_password(self, dba):
+    #     password = 'g00d p4ssw0rd'
+    #     salt = b'SBHe5zI8OkN94rsVHSZE3dwAvimz-ukl11ONcJgEAbo='
+    #     dba.add_password(password, salt)
+
+    def test_decrypt_key(self, dba):
+        password = 'g00d p4ssw0rd'
+        salt = b'SBHe5zI8OkN94rsVHSZE3dwAvimz-ukl11ONcJgEAbo='
+        rowid = 5
+        f = dbs.cs.do_crypto_stuff(password, salt, 200_000)
+        key = dbs.Fernet.generate_key()
+        encrypted_key = f.encrypt(key)
+        dba.update_by_rowid(dba.table, (dba.cols[1],), (encrypted_key,), rowid)
+
+        # check that we can decrypt the key
+        row = dba.select_row_by_rowid(dba.table, rowid)
+        # assert row[2] == encrypted_key
+        assert dba.decrypt_key(row[2], password, salt) == key
+
+
+class TestDBkeys():
+    pass
+
+
+class TestDBpassword():
+    pass
