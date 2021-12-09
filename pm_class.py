927c3e417890e9ebdd9ddb962c5691177c23521a
Heikki Jylhä <hjjylh@gmail.com>
2021-12-09 18:12:49 +0200

    added a lot of password methods

--- pm_class.py
+++ pm_class.py
@@ -1,6 +1,7 @@
 
 import dbs
 from dbs import PasswordNotFoundError, DB_auth, DB_keys, DB_password
+from tests.test_dbs import rows_and_keys
 
 
 class PM():
@@ -44,5 +45,88 @@ class PM():
             # self.check_master_password(old_password)
             master_key = self.dba.decrypt_key(e_master_key, old_password, self.get_salt())
         self.master_key = self.dba.add_password(new_password, self.get_salt(), master_key, rowid)
+
+    
+    # into the real business
+    # app_or_email = 0 for app, 1 for email
+    def find_info(self, app_or_email, name):
+        if not self.master_key:
+            raise Exception('Master key not active')
+        # get in-use rows and corresponding keys
+        rows_and_keys = self.dbk.get_rows_and_keys(app_or_email, self.master_key)
+        # search through the rows
+        return self.dbp.find(app_or_email, name, rows_and_keys)
+
+    # app_or_email = 0 for app, 1 for email
+    def add_info(self, app_or_email, name):
+        if not self.master_key:
+            raise Exception('Master key not active')
+        # check if name is already in db (not sure if this should be part of UI)
+        list_of_similar_names = (name for _, name in self.find_info(app_or_email, name))
+        if name in list_of_similar_names:
+            return False
+        # if name is not in db, add it
+        row_and_key = self.dbk.add_new_key(app_or_email, self.master_key)
+        
+        # try:
+        self.dbp.insert_data(app_or_email, (name, str(0)), row_and_key)
+        # except Exception as exc:
+        #     print('Exception', exc)
+        #     print('This should be 2:', len(row_and_key))
+        #     print('rowid:', row_and_key[0])
+        #     print('key:', row_and_key[1])
+        return True
+
+    # check that no invalid tokens exist
+    def check_keys(self):
+        if not self.master_key:
+            raise Exception('Master key not active')
+        pass
+
+    # get the possible rowids for app and email
+    def prepare_to_add_password(self, app: str, email: str) -> list:
+        findings = {app: self.find_info(0, app)}
+        findings[email] = self.find_info(1, email)
+        return findings
+
+    # TODO: this should check if the data already exists
+    def add_password(self, username: str, email_rowid: int, password: str, app_rowid: int, url: str) -> None:
+        if not self.master_key:
+            raise Exception('Master key not active')
+        # add new key
+        row_and_key = self.dbk.add_new_key(2, self.master_key)
+        # add some zeros to mess with numbers for no reason
+        email = ''.join(['0' for _ in range(dbs.cs.secrets.randbelow(23))] + [str(email_rowid)])
+        app = ''.join(['0' for _ in range(dbs.cs.secrets.randbelow(23))] + [str(app_rowid)])
+        data = (username, email, password, app, url)
+        self.dbp.insert_data(2, data, row_and_key)
+
+    def find_password(self, app: str) -> list:
+        if not self.master_key:
+            raise Exception('Master key not active')
+        app_info = self.find_info(0, app)
+        # print(f'{app_info=}')
+        password_info = []
+        for info in app_info:
+            # print('app info has rowid:', info[0])
+            pw_rows = self.dbp.find_password(0, info[0], self.dbk.get_rows_and_keys(2, self.master_key))
+            # print(f'{pw_rows=}')
+            password_info += pw_rows
+        return password_info
+
+    # changing and removing password depends on the rowid
+    def change_password(self, rowid, new_password):
+        if not self.master_key:
+            raise Exception('Master key not active')
+        row_and_key = (rowid, self.dbk.get_rows_and_keys(2, self.master_key)[rowid])
+        self.dbp.change_password(new_password, row_and_key)
+
+    def delete_password(self, rowid):
+        if not self.master_key:
+            raise Exception('Master key not active')
+        # overwrite the row
+        self.dbp.delete_data(2, rowid)
+        # remove the corresponding key and change it to be vacant
+        self.dbk.remove_key(2, rowid)
         
     
--- tests/test_pm_class.py
+++ tests/test_pm_class.py
@@ -8,6 +8,16 @@ import dbs
 from pm_class import PM, DB_auth, DB_keys, DB_password, PasswordNotFoundError
 
 
+# get some dummy app names and email addresses
+@pytest.fixture
+def app_list():
+    return [f'app{i}' for i in range(100)]
+
+@pytest.fixture
+def email_list():
+    return [f'email{i}@provider.com' for i in range(100)]
+
+
 # db with only 1 row of dummy data
 @pytest.fixture
 def pm_temp():
@@ -17,11 +27,13 @@ def pm_temp():
     yield PM(DB_auth(db_path), DB_keys(db_path), DB_password(db_path))
     db_path.unlink()
 
+# "proper" db
 @pytest.fixture
 def pm():
     db_path = Path(__file__).parent / 'test_db_pm.db'
     return PM(DB_auth(db_path), DB_keys(db_path), DB_password(db_path))
 
+# db with master_key "active"
 @pytest.fixture
 def pm_w_master_key(pm):
     master_password = '0K_p455w0rd'
@@ -59,33 +71,118 @@ class TestwTempDB():
         password = 'test_password'
         pm_temp.add_master_password(password)
         assert pm_temp.check_master_password(password)
+    
+    def test_master_key_fail(self, pm_temp):
+        with pytest.raises(Exception):
+            pm_temp.find_info(0, 'whatever')
+        with pytest.raises(Exception):
+            pm_temp.find_info(1, 'whatever')
+        with pytest.raises(Exception):
+            pm_temp.add_info(0, 'whatever')
+        with pytest.raises(Exception):
+            pm_temp.add_info(1, 'whatever')
+
+class TestChangePassword():
+    def test_change_master_password(self, pm):
+        # pw = 'original pw'
+        pw = dbs.cs.generate_password()
+        pm.add_master_password(pw)
+
+        # going deep to check things
+        rowid, e_key = pm.dba.check_password(pw, True)
+        row = pm.dba.select_row_by_rowid(pm.dba.table, rowid)
+        ph = dbs.cs.PasswordHasher()
+        assert ph.verify(row[1], pw)
+        assert row[2] == e_key
+        master_key = pm.dba.decrypt_key(e_key, pw, pm.get_salt())
+        assert master_key == pm.master_key
+
+        # new_pw = 'new pw'
+        new_pw = dbs.cs.generate_password()
+        pm.change_master_password(pw, new_pw, False)
+        assert master_key == pm.master_key
+        row = pm.dba.select_row_by_rowid(pm.dba.table, rowid)
+        assert ph.verify(row[1], new_pw)
+        assert master_key == pm.dba.decrypt_key(row[2], new_pw, pm.get_salt())
+
+        newer_pw = dbs.cs.generate_password()
+        pm.change_master_password(new_pw, newer_pw)
+        assert master_key != pm.master_key
+        row = pm.dba.select_row_by_rowid(pm.dba.table, rowid)
+        assert ph.verify(row[1], newer_pw)
+        assert pm.master_key == pm.dba.decrypt_key(row[2], newer_pw, pm.get_salt())
+
+    
+class TestPasswordManagement():
+    
+    def test_add_info(self, pm_w_master_key):
+        app = 'good app'
+        pm_w_master_key.add_info(0, app)
+        info = pm_w_master_key.find_info(0, app)
+        assert len(info) == 1
+        assert app in [a for _, a in info]
+
+        email = 'zoomin@place.org'
+        pm_w_master_key.add_info(1, email)
+        info = pm_w_master_key.find_info(1, email)
+        assert len(info) == 1
+        assert email in [a for _, a in info]
+
+    def test_find_info(self, pm_w_master_key):
+        app = 'not here'
+        assert pm_w_master_key.find_info(0, app) == []
+        assert pm_w_master_key.find_info(1, app) == []
+        # this should have been added
+        app = 'good app'
+        assert pm_w_master_key.find_info(0, app)[0][1] == app
+
+    def test_prepare_to_add_password(self, pm_w_master_key):
+        app = 'good app'
+        email = 'zoomin@place.org'
+        infos = pm_w_master_key.prepare_to_add_password(app, email)
+        assert len(infos[app]) == 1
+        assert len(infos[email]) == 1
+
+    def test_add_password(self, pm_w_master_key):
+        app = 'good app'
+        email = 'zoomin@place.org'
+        username = 'user001'
+        password = 'no effort'
+        url = 'goodapp.com'
+        infos = pm_w_master_key.prepare_to_add_password(app, email)
+        a = infos[app][0][0]
+        e = infos[email][0][0]
+        pm_w_master_key.add_password(username, e, password, a, url)
+        # see if the info can be found
+        pw_infos = pm_w_master_key.find_password(app)
+        assert pw_infos[0][1] == username
+        assert pw_infos[0][2] == e
+        assert pw_infos[0][3] == password
+        assert pw_infos[0][4] == a
+        assert pw_infos[0][5] == url
+
+    # def test_find_password(self, pm_w_master_key):
+    #     pass
+        
+
+    def test_change_password(self, pm_w_master_key):
+        app = 'good app'
+        pw_infos = pm_w_master_key.find_password(app)
+        rowids = tuple(row[0] for row in pw_infos)
+        if not len(rowids) == 1:
+            raise Exception('too many inserts...')
+        rowid = rowids[0]
+        new_password = 'more effort'
+        pm_w_master_key.change_password(rowid, new_password)
+        # checking
+        assert pm_w_master_key.find_password(app)[0][3] == new_password
 
 
-def test_change_master_password(pm):
-    # pw = 'original pw'
-    pw = dbs.cs.generate_password()
-    pm.add_master_password(pw)
-
-    # going deep to check things
-    rowid, e_key = pm.dba.check_password(pw, True)
-    row = pm.dba.select_row_by_rowid(pm.dba.table, rowid)
-    ph = dbs.cs.PasswordHasher()
-    assert ph.verify(row[1], pw)
-    assert row[2] == e_key
-    master_key = pm.dba.decrypt_key(e_key, pw, pm.get_salt())
-    assert master_key == pm.master_key
-
-    # new_pw = 'new pw'
-    new_pw = dbs.cs.generate_password()
-    pm.change_master_password(pw, new_pw, False)
-    assert master_key == pm.master_key
-    row = pm.dba.select_row_by_rowid(pm.dba.table, rowid)
-    assert ph.verify(row[1], new_pw)
-    assert master_key == pm.dba.decrypt_key(row[2], new_pw, pm.get_salt())
-
-    newer_pw = dbs.cs.generate_password()
-    pm.change_master_password(new_pw, newer_pw)
-    assert master_key != pm.master_key
-    row = pm.dba.select_row_by_rowid(pm.dba.table, rowid)
-    assert ph.verify(row[1], newer_pw)
-    assert pm.master_key == pm.dba.decrypt_key(row[2], newer_pw, pm.get_salt())
+    def test_delete_password(self, pm_w_master_key):
+        app = 'good app'
+        pw_infos = pm_w_master_key.find_password(app)
+        rowids = tuple(row[0] for row in pw_infos)
+        # remove all passwords related to app
+        for r in rowids:
+            pm_w_master_key.delete_password(r)
+        assert pm_w_master_key.find_password(app) == []
