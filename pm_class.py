8f0a0d4241df31c47694dc26fcc917633bbff525
Heikki Jylhä <hjjylh@gmail.com>
2021-12-09 11:24:38 +0200

    master password methods done, probably

--- pm_class.py
+++ pm_class.py
@@ -1,6 +1,5 @@
 
-# import dbs
-from typing import ParamSpec
+import dbs
 from dbs import PasswordNotFoundError, DB_auth, DB_keys, DB_password
 
 
@@ -14,10 +13,20 @@ class PM():
     
     # get salt (from somewhere in the files...)
     def get_salt(self):
+        # TODO: all of these salt things
+        return b'\xc1\x95\xe15=\tm\xef\xecTH\x8e\xf5;/l'
+
+    # generate salt (and put it in some file)
+    def generate_salt(self):
         pass
+
+    # add password (and generate master key) the first time
+    def add_master_password(self, password):
+        self.master_key = self.dba.add_password(password, self.get_salt())
+        # self.master_key = self.dba.decrypt_key(encrypted_m_key, password, self.get_salt())
     
     # check that password is in db, and get master key
-    def check_password(self, password):
+    def check_master_password(self, password):
         try:
             encrypted_m_key = self.dba.check_password(password)
             self.master_key = self.dba.decrypt_key(encrypted_m_key, password, self.get_salt())
@@ -27,5 +36,13 @@ class PM():
             # print('Password not in database.')
             return False
         
+    # change the password (and change master key, optionally)
+    def change_master_password(self, old_password, new_password, change_master_key=True):
+        rowid, e_master_key = self.dba.check_password(old_password, True)
+        master_key = None
+        if not change_master_key:
+            # self.check_master_password(old_password)
+            master_key = self.dba.decrypt_key(e_master_key, old_password, self.get_salt())
+        self.master_key = self.dba.add_password(new_password, self.get_salt(), master_key, rowid)
         
     
--- tests/test_pm_class.py
+++ tests/test_pm_class.py
@@ -1,7 +1,91 @@
+from pathlib import Path
+import time
+
 import pytest
 
-
 import fix_imports
+import dbs
+from pm_class import PM, DB_auth, DB_keys, DB_password, PasswordNotFoundError
 
-from pm_class import PM
 
+# db with only 1 row of dummy data
+@pytest.fixture
+def pm_temp():
+    db_path = Path(__file__).parent / 'test_db_temp.db'
+    for t in ('auth', 'keys', 'password'):
+        dbs.initiate_db(db_path, t, 1)
+    yield PM(DB_auth(db_path), DB_keys(db_path), DB_password(db_path))
+    db_path.unlink()
+
+@pytest.fixture
+def pm():
+    db_path = Path(__file__).parent / 'test_db_pm.db'
+    return PM(DB_auth(db_path), DB_keys(db_path), DB_password(db_path))
+
+@pytest.fixture
+def pm_w_master_key(pm):
+    master_password = '0K_p455w0rd'
+    if not pm.check_master_password(master_password):
+        pm.add_master_password(master_password)
+    return pm
+
+
+def test_init(pm):
+    # check some methods of different classes
+    assert pm.dba.check_uniqueness_of_keys()
+    # pm.dbk
+
+class TestwTempDB():
+    def test_add_master_password(self, pm_temp):
+        password = 'pw is good'
+        pm_temp.add_master_password(password)
+        # check what happened to the one row
+        row = pm_temp.dba.select_all(pm_temp.dba.table)[0]
+        # check password
+        ph = dbs.cs.PasswordHasher()
+        assert ph.verify(row[1], password)
+        # check encryption
+        f = dbs.cs.do_crypto_stuff(password, pm_temp.get_salt(), 200_000)
+        assert pm_temp.master_key == f.decrypt(row[2])
+        # hashing takes time, but not more than 2 seconds
+        time_in_db = int(dbs.cs.decrypt_text(row[3], f))
+        assert int(time.time()) - time_in_db < 3
+
+
+    def test_check_master_password(self, pm_temp):
+        password = 'not gonna be found'
+        assert not pm_temp.check_master_password(password)
+        
+        password = 'test_password'
+        pm_temp.add_master_password(password)
+        assert pm_temp.check_master_password(password)
+
+
+def test_change_master_password(pm):
+    # pw = 'original pw'
+    pw = dbs.cs.generate_password()
+    pm.add_master_password(pw)
+
+    # going deep to check things
+    rowid, e_key = pm.dba.check_password(pw, True)
+    row = pm.dba.select_row_by_rowid(pm.dba.table, rowid)
+    ph = dbs.cs.PasswordHasher()
+    assert ph.verify(row[1], pw)
+    assert row[2] == e_key
+    master_key = pm.dba.decrypt_key(e_key, pw, pm.get_salt())
+    assert master_key == pm.master_key
+
+    # new_pw = 'new pw'
+    new_pw = dbs.cs.generate_password()
+    pm.change_master_password(pw, new_pw, False)
+    assert master_key == pm.master_key
+    row = pm.dba.select_row_by_rowid(pm.dba.table, rowid)
+    assert ph.verify(row[1], new_pw)
+    assert master_key == pm.dba.decrypt_key(row[2], new_pw, pm.get_salt())
+
+    newer_pw = dbs.cs.generate_password()
+    pm.change_master_password(new_pw, newer_pw)
+    assert master_key != pm.master_key
+    row = pm.dba.select_row_by_rowid(pm.dba.table, rowid)
+    assert ph.verify(row[1], newer_pw)
+    assert pm.master_key == pm.dba.decrypt_key(row[2], newer_pw, pm.get_salt())
