2ae09a8266f3b5741f3d727818274b6ed145f329
Heikki Jylhä <hjjylh@gmail.com>
2021-12-12 16:34:48 +0200

    added storing and obtaining the filepaths for dbs and salt

---
+++ file_handling.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+
+import file_locations
+
+
+def get_drives():
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return [f"{c}:" for c in letters if Path(f"{c}:").exists()]
+
+def generate_salt(salt_path):
+    with open(salt_path, 'wb') as f:
+        f.write(os.urandom(16))
+
+def get_salt(salt_path):
+    with open(salt_path, 'rb') as f:
+        return f.read()
+
+def get_files():
+    # salt
+    salt_path = Path(file_locations.salt_path)
+    if not salt_path.exists():
+        generate_salt(salt_path)
+    salt = get_salt(salt_path)
+    # dbs
+    auth_path = Path(file_locations.db_auth_path)
+    keys_path = Path(file_locations.db_keys_path)
+    data_path = Path(file_locations.db_data_path)
+    exists = auth_path.exists() and keys_path.exists() and data_path.exists()
+    return ((salt, auth_path, keys_path, data_path), exists)
+    
---
+++ file_locations.py
@@ -0,0 +1,6 @@
+# default locations, obviously these should be spread around
+
+salt_path = 'data/salt'
+db_auth_path = 'data/auth.db'
+db_keys_path = 'data/keys.db'
+db_data_path = 'data/data.db'
