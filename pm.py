39ffb20c38ee13091f6879b953f479a80af2851b
Heikki Jylhä <hjjylh@gmail.com>
2021-12-12 17:10:18 +0200

    new db structure is live

--- pm.py
+++ pm.py
@@ -2,62 +2,49 @@
 # import os
 import getpass
 # import pyperclip
-from crypto_stuff import do_crypto_stuff, create_hash_storage
-from db import check_db, get_master_table
-import crypto_db
-from pm_setup import load_salt, initiate_db
-from pm_data import default_db_filename, default_salt_filename
-import pm_fcns as pm
-
-db_filename = default_db_filename
-salt_filename = default_salt_filename
+# from crypto_stuff import do_crypto_stuff, create_hash_storage
+# from db import check_db, get_master_table
+# import crypto_db
+# from pm_setup import load_salt, initiate_db
+# from pm_data import default_db_filename, default_salt_filename
+# import pm_fcns as pm
+from pm_ui import PM_UI, clear_clipboard, clear_screen
 
 # ask to press enter, and clear clipboard
 def end_prompt():
     print('\n')
     getpass.getpass('Press ENTER to return back to menu. (This also clears clipboard)')
-    pm.clear_clipboard()
+    clear_clipboard()
     print('\n')
 
 
-def password_manager(db_filename=default_db_filename, salt_filename=default_salt_filename):
+def password_manager():
     # starting text
-    print('Welcome to my Password Manager\n')
+    # print('Welcome to my Password Manager\n')
+    pm_ui = PM_UI()
 
-    # check if DB has been initiated
-    # is_initiated = check_db(db_filename)
-    if check_db(db_filename):
-        # if initiated, ask for username and password
-        username, master_password = pm.ask_for_username_and_password()
-        # username = input('Enter your username: ')
-        # master_password = getpass.getpass(prompt='Enter the master password: ')
-        f = do_crypto_stuff(master_password, load_salt(salt_filename), 100000)
-        # check that they are in the database
-        # master_d = crypto_db.get_master_table(master_password, f)
-        master_h = get_master_table(db_filename)
-        un_h = create_hash_storage(username, load_salt(salt_filename))
-        pw_h = create_hash_storage(master_password, load_salt(salt_filename))
-        # if not(username == master_d[0] and master_password == master_d[1]):
-        if not(un_h == master_h[0] and pw_h == master_h[1]):
-            print('Invalid password or username')
-            exit()
-    else:
-        print('Password database has not been initiated yet.')
-        # print('Do you want to initiate it? (Y/N)')
-        answer = pm.yes_or_no_question('Do you want to initiate it?')
-        if answer.lower() == 'y':
-            print('\n')
-            initiation_data = initiate_db()
-            username = initiation_data[0]
-            master_password = initiation_data[1]
-            f = initiation_data[2]
+    if pm_ui.pm is None:
+        print('Not all the necessary files are present.')
+        ans = pm_ui.yes_or_no_question('Do you want to initialize the Password Manager?')
+        if ans.lower() == 'y':
+            pm_ui.initiate_pm()
+            clear_screen()
         else:
             exit()
+    
+    un = 'placeholder username'
+    if pm_ui.pm.master_key is None:
+        un, master_pw = pm_ui.ask_for_username_and_password()
+        if pm_ui.pm.check_master_password(master_pw):
+            pm_ui.pm.set_name_lists()
+        else:
+            print('Incorrect username and password')
+            exit()
 
-    # maybe clear screen before starting
-    pm.clear_screen()
-    master_password = None
-    print(f'Password Manager for user {username}\n')
+    # no need for master pw anymore
+    master_pw = None
+    clear_screen()
+    print(f'Password Manager for user {un}\n')
 
     # the program loop
     while True:
@@ -65,31 +52,42 @@ def password_manager(db_filename=default_db_filename, salt_filename=default_salt
         print('(1) Add a new password')
         print('(2) Change a password')
         print('(3) Retrieve a password')
-        print('(4) Delete a password')
+        # TODO: this one
+        print('(4) Change information related to a password (NOT WORKING ATM)')
         print('(5) Show a list of apps with passwords')
+        print('(7) Delete a password')
+        print('(9) Clear Screen')
         print('(0) Exit')
         action = input()
         if action == '0':
-            exit()
+            ans = pm_ui.yes_or_no_question('Are you sure you want to exit Password Manager?')
+            if ans.lower() == 'y':
+                exit()
         elif action == '1':
-            pm.add_password(f)
+            pm_ui.add_password()
             end_prompt()
         elif action == '2':
-            pm.change_password(f)
+            pm_ui.change_password()
             end_prompt()
         elif action == '3':
-            pm.find_password_for_app(f)
+            pm_ui.find_password_for_app()
             end_prompt()
         elif action == '4':
-            pm.delete_password(f)
+            print('Action under construction...')
+            # TODO
             end_prompt()
         elif action == '5':
             print('These are the apps you have saved passwords for:')
-            app_list = crypto_db.get_app_list(f)
+            app_list = [app for _, app in pm_ui.pm.get_name_list(0)]
             for app in app_list:
                 print('\t', app)
             print('\n')
             end_prompt()
+        elif action == '7':
+            pm_ui.delete_password()
+            end_prompt()
+        elif action == '9':
+            clear_screen()
         else:
             print('Invalid input. Please choose one of the options.\n')
 
--- pm_data.py
+++
@@ -1,3 +0,0 @@
-default_db_filename = "pw.db"
-default_salt_filename = "salt.txt"
-salt_thingie = '2Xi8Cl80nzkx6ohqmi0NE3BcytBsanqwGMZ-DGwN4Zo='
