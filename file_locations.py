5277676be366fee1c09eea8ca06836532001bdf5
Heikki Jylhä <hjjylh@gmail.com>
2021-12-13 12:59:30 +0200

    added file extension to salt file

--- file_locations.py
+++ file_locations.py
@@ -1,6 +1,6 @@
 # default locations, obviously these should be spread around
 
-salt_path = 'data/salt'
+salt_path = 'data/salt.txt'
 db_auth_path = 'data/auth.db'
 db_keys_path = 'data/keys.db'
 db_data_path = 'data/data.db'
